aa53a32b2200f28f604f0ed794df5607c404c3ec
Damian Galka <damian.galka@sabre.com>
2021-01-28 17:55:32 +0100

    1#card-53650999: Generate the pie charts

--- scripts/export-utils.sh
+++ scripts/export-utils.sh
@@ -27,22 +27,34 @@ export_create_file_name() {
 
 export_collect_output() {
   _files=()
+  declare -A _charts_data
   while read -r _line; do
     echo "$_line"
     if [[ -n "$EXPORT_ACTIVATED" ]]; then
       EXPORT_DIR=$(export_get_dir)
       mkdir -p "$EXPORT_DIR"
       if [[ "$_line" =~ $__title_regex ]]; then
-        log_debug "Matched title: ${BASH_REMATCH[1]}"
-        _csv_file="$(export_create_file_name "${BASH_REMATCH[1]}").csv"
+        _title="${BASH_REMATCH[1]}"
+        log_debug "Matched title: $_title"
+        _csv_file="$(export_create_file_name "$_title").csv"
+        _png_file="$(export_create_file_name "$_title").png"
+        _charts_data[$_png_file]="$_title|"
         touch "$EXPORT_DIR/$_csv_file"
-        _files+=("$EXPORT_DIR/$_csv_file")
+        _files+=("$_csv_file", "$_png_file")
       else
         echo "$_line" >>"$EXPORT_DIR/$_csv_file"
+        if [[ "$_line" != *"#"* ]]; then
+          _charts_data[$_png_file]+="${_line//,/:},"
+        fi
       fi
     fi
   done
   if [[ -n "$EXPORT_ACTIVATED" ]]; then
-    echo "Data exported to: ${_files[*]}"
+    for key in "${!_charts_data[@]}"; do
+      _details="${_charts_data[$key]//*|/}"
+      log_debug "/apps/git-stats/scripts/generate_chart.py '${_charts_data[$key]//|*/}' '$EXPORT_DIR/$key' '${_details::-1}'"
+      /apps/git-stats/scripts/generate_chart.py "${_charts_data[$key]//|*/}" "$EXPORT_DIR/$key" "${_details::-1}"
+    done
+    echo "Generated in $EXPORT_DIR files: ${_files[*]}"
   fi
 }
--- scripts/generate_chart.py
+++ scripts/generate_chart.py
@@ -2,12 +2,6 @@
 import sys
 from pygooglechart import PieChart2D
 
-# Count the arguments
-arguments = len(sys.argv) - 1
-print("The script is called with %i arguments" % (arguments))
-for arg in sys.argv[1:]:
-    print("Argument: %s" % (arg))
-
 title = sys.argv[1]
 output_file = sys.argv[2]
 data_str = sys.argv[3]
@@ -27,4 +21,9 @@ for d in data:
 chart = PieChart2D(600, 200, title, legend)
 chart.add_data(data)
 chart.set_pie_labels(labels)
-chart.download(output_file)
+while True:
+    try:
+        chart.download(output_file)
+    except:
+        continue
+    break
